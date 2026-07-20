07c10cf587f3aa90ce500061b5c695bdf7cf8837
swapnilrob <swapnilrob.27@gmail.com>
2026-07-21 01:56:27 +0600

    Add Notification schema

---
+++ server/models/Notification.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema(
+  {
+    recipient: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        'analysis_complete',
+        'batch_complete',
+        'report_shared',
+        'new_comment',
+        'announcement',
+      ],
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+notificationSchema.index({ recipient: 1, isRead: 1 });
+
+module.exports = mongoose.model('Notification', notificationSchema);
