3db14e8bdeedf7e9b41e5df2d17d8ae27d0c0398
swapnilrob <swapnilrob.27@gmail.com>
2026-07-21 02:39:07 +0600

    Add password hashing and comparison method to User schema

--- server/models/User.js
+++ server/models/User.js
@@ -50,4 +50,19 @@ const userSchema = new mongoose.Schema(
   }
 );
 
+const bcrypt = require('bcryptjs');
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
 module.exports = mongoose.model('User', userSchema);
